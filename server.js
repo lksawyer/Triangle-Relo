f46b621b52406ce0eeaf5602ea1f17eee12ddcb6
derejohn <djsaugatuck@yahoo.com>
2018-03-10 10:26:39 -0500

    add mongo1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+//var mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -8,3 +8,14 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+/// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/wakeHomes";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+//mongoose.Promise = Promise;
+//mongoose.connect(MONGODB_URI, {
+//  useMongoClient: true
+//});
