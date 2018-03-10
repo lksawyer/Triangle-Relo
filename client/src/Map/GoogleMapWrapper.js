76a22232bf25a65e95c03427d6e13b01fbc5e744
Joshua Gomez <joshuagomez@Joshuas-MacBook-Pro.local>
2018-03-10 10:25:20 -0500

    added initial code for googlesearch

--- client/src/Map/GoogleMapWrapper.js
+++ client/src/Map/GoogleMapWrapper.js
@@ -18,6 +18,41 @@ class GoogleMapWrapper extends Component {
                 map: this.map
             });
         }
+
+           //sets up seeing nearby amenities, within [x] radius
+           var service;
+           var infowindow;
+   
+           function amenities() {
+               var location = new google.maps.LatLng({lat: parseFloat(nextProps.newCord.lat), lng: parseFloat(nextProps.newCord.lng)});
+              
+               var request = {
+                 location: location,
+                 radius: '500',
+                 type: ['restaurant']
+               };
+             
+               service = new google.maps.places.PlacesService(this.map);
+               service.nearbySearch(request, callback);
+             }
+             
+             function callback(results, status) {
+               if (status == google.maps.places.PlacesServiceStatus.OK) {
+                 for (var i = 0; i < results.length; i++) {
+                   var place = results[i];
+                   createMarker(results[i]);
+                 }
+               }
+             }
+   
+             function createMarker(place) {
+               var placeLoc = place.geometry.location;
+               var marker = new google.maps.Marker({
+                 map: this.map,
+                 position: place.geometry.location
+               });
+       
+             }
     }
 
     componentDidMount() {
