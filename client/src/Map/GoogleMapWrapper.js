023af85862cfaee298f52c9dc582aeb37b9c0986
lksawyer <loganksawyer@gmail.com>
2018-03-11 18:49:44 -0400

    added autocomplete func to search, got places markers to 'work'

--- client/src/Map/GoogleMapWrapper.js
+++ client/src/Map/GoogleMapWrapper.js
@@ -19,47 +19,61 @@ class GoogleMapWrapper extends Component {
             });
         }
 
-           //sets up seeing nearby amenities, within [x] radius
-           var service;
-           var infowindow;
-   
-           function amenities() {
-               var location = new google.maps.LatLng({lat: parseFloat(nextProps.newCord.lat), lng: parseFloat(nextProps.newCord.lng)});
-              
-               var request = {
-                 location: location,
-                 radius: '500',
-                 type: ['restaurant']
-               };
-             
-               service = new google.maps.places.PlacesService(this.map);
-               service.nearbySearch(request, callback);
-             }
-             
-             function callback(results, status) {
-               if (status == google.maps.places.PlacesServiceStatus.OK) {
-                 for (var i = 0; i < results.length; i++) {
-                   var place = results[i];
-                   createMarker(results[i]);
-                 }
-               }
-             }
-   
-             function createMarker(place) {
-               var placeLoc = place.geometry.location;
-               var marker = new google.maps.Marker({
-                 map: this.map,
-                 position: place.geometry.location
-               });
-       
-             }
     }
 
     componentDidMount() {
+
+        const center = {lat: this.props.defaultLat, lng: this.props.defaultLng}; 
+
         this.map = new google.maps.Map(this.refs.map, {
-            center: {lat: this.props.defaultLat, lng: this.props.defaultLng},
+            center: center,
             zoom: 11
-        })
+        });
+
+        //Places=================================================
+
+        //where to search for nearby places, radius of search, type of place (grocery, cafe, etc)
+        const request = {
+            query: 'cafe',
+            location: center,
+            radius: 8047,
+        };
+
+        //Construct a service object
+        const service = new google.maps.places.PlacesService(this.map);
+
+        //Callback for text search. Ensures places are returned
+        const callback = (results, status) => {
+            if(status == google.maps.places.PlacesServiceStatus.OK) {
+                for (let i=0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+        }
+        
+        //Custom maker icon
+        const image = {
+            url: "https://www.shareicon.net/data/2015/09/21/644139_pin_512x512.png",
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+        const createMarker = (place) => {
+            const placeLoc = place.geometry.location;
+            const marker = new google.maps.Marker({
+                map: this.map,
+                position: place.geometry.location,
+                title: "cafe",
+                icon: image
+            });
+        }
+
+        service.textSearch(request, callback);
+
+        //Places=================================================
+
     }
 
     render() {
