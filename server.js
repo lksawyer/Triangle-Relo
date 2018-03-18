fe7dbe89a588012b66947cf584adfba168b7061c
shulme801 <shulme801@gmail.com>
2018-03-18 13:43:12 -0400

    Fix server.js startup error

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+console.log("Port is "+PORT);
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,16 +16,25 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Require all models
-var db = require("./models");
+// var db = require("./models");
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/WakeHomes",
-  {
-    
-  }
-);
+// If deployed, use the deployed database. Otherwise use the local mongo nytscrape database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WakeHomes";
+mongoose.connect(MONGODB_URI, { useMongoClient: true });
+
+const db = mongoose.connection;
+
+// Show any Mongoose errors
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
